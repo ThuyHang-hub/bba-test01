62c4a6bba1488b2e4bad9483601f60b6dd0885fd
test 01 - Thuy Hang <thuyhanghmu@gmail.com>
2025-09-28 00:46:07 +0700

    feat: add solution for challenge 05

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,17 @@
+
+function findPairsDivisibleBy17 () {
+    let n = 0;
+    for (let c = 1; c <= 100; c++) {
+        for (let d = 100; d >= 1; d--) {
+            let sum = c + d;
+            if (sum % 17 === 0 && c !== d) {
+                console.log("Cặp số thứ", n, ":", c ,"và", d);
+                n++;
+            }
+        }
+    }
+    console.log("Tổng số:", n, "cặp");
+};
+findPairsDivisibleBy17();
+
+
